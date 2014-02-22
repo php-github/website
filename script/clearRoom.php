5037ce8d458fd4d8816587e33d3a567673445c7f
lifenglin <364613883@qq.com>
2014-02-22 14:55:52 +0800

    清理无用

--- script/clearRoom.php
+++ script/clearRoom.php
@@ -1,6 +1,8 @@
 <?php
 require(__DIR__ . '/../webroot/init.php');
 $objClub = new Tofu\Model('club', 'room');
+echo $objClub->count();
+exit;
 $objClub->remove(array(), array("justOne" => false));
 echo $objClub->count();
 exit;
